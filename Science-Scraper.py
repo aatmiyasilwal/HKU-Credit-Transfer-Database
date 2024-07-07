b69ba9fcd35d1488c291b5fbc573867982a5a6fb
Aatmiya Silwal <aatmiyasilwal1@gmail.com>
2024-07-07 23:22:08 +0800

    Toxic commit for science faculty check

---
+++ Science-Scraper.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+#service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Safari()
+
+driver.get("https://webapp.science.hku.hk/student/servlet/course_equiv")
+
+df = pd.DataFrame(columns=[
+    'University',
+    'University-Country',
+    'Exchange-Course-Code',
+    'Exchange-Course-Title',
+    'HKU-Course-Code',
+    'HKU-Course-Title'
+])
+
+check_code = "EXU001"
+uni_names=driver.find_element(By.ID, check_code)
+
+
+'''uni_number = 0
+
+table_elements = driver.find_elements(By.CLASS_NAME, "uni")
+
+for table_element in table_elements:
+    print(uni_number)
+    current_uni = uni_names[uni_number].text.split(" - ")[0]
+    uni_country = uni_names[uni_number].text.split(" - ")[1]
+
+    rows = table_element.find_elements(By.TAG_NAME, "tr")
+
+    for row in rows[2:]:
+        cells = row.find_elements(By.TAG_NAME, "td")
+        
+        new_row = {
+            'University': current_uni,
+            'University-Country': uni_country,
+            'Exchange-Course-Code': cells[0].text,
+            'Exchange-Course-Title': cells[1].text,
+            'HKU-Course-Code': cells[3].text,
+            'HKU-Course-Title': cells[4].text
+        }
+
+        # Append the new row to the DataFrame
+        df.loc[len(df)] = new_row
+
+    uni_number += 1
+
+print(df)
+'''
+driver.quit()
+
+df.to_csv("sosci-credit_transfer_database.csv", index=False)
