4104d20dd7e53225004ae05fca5ec90c5f93a74a
Aatmiya Silwal <aatmiyasilwal1@gmail.com>
2025-02-01 16:47:03 -0700

    missing piece of the puzzle solved

--- Science-Scraper.py
+++ Science-Scraper.py
@@ -18,9 +18,8 @@ df = pd.DataFrame(columns=[
 count = 0
 driver.get("https://webapp.science.hku.hk/student/servlet/course_equiv")
 
-select_element = Select(driver.find_element(By.NAME, "ex_ucode"))
-options = select_element.options
-
+init = Select(driver.find_element(By.NAME, "ex_ucode"))
+options = init.options
 count = len(options) - 1
 
 while count >= 0:
@@ -29,17 +28,29 @@ while count >= 0:
     curr_option = curr_elem_node.options[count]
     val = curr_option.get_attribute("value")
 
-    if val:  
-        curr_elem_node.select_by_value(val)  
+    if val:
+        info = curr_option.text
+        country_start, country_end = curr_option.text.index(
+            ".") + 2, curr_option.text.index("-") - 1
+        curr_elem_node.select_by_value(val)
         time.sleep(2)
-    
-        table_rows = driver.find_elements(By.XPATH, "//table[@class='msg']/tbody/tr")
-        for row in table_rows[1:]:  # Skip header row
+
+        table_rows = driver.find_elements(
+            By.XPATH, "//table[@class='msg']/tbody/tr")
+        for row in table_rows[1:]:
             cells = row.find_elements(By.TAG_NAME, "td")
-            print([cell.text for cell in cells])
-    
-    print("*" * 20)
-    
+            if cells[0].text == "\n\t\t":
+                break
+            new_row = {
+                'University': info[country_end + 2:].strip(""),
+                'University-Country': info[country_start:country_end].strip(),
+                'Exchange-Course-Code': cells[1].text.strip(),
+                'Exchange-Course-Title': cells[3].text.strip(""),
+                'HKU-Course-Code': cells[4].text.strip(),
+                'HKU-Course-Title': cells[6].text.strip()
+            }
+            df.loc[len(df)] = new_row
     count -= 1
 
 driver.quit()
+df.to_csv("science-credit_transfer_database.csv", index=False)
